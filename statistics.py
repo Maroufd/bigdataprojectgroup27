1afacfd60dba4aa649a63f2327351a8b2ffda4e5
LuciaCatalan <55504760+LuciaCatalan@users.noreply.github.com>
2021-12-03 16:57:51 +0100

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,4 +1,8 @@
-from pyspark import *
+#################
+# STATISTICS.PY #
+#################
+
+from pyspark import *	
 from pyspark.sql.context import SQLContext
 from pyspark.sql.types import IntegerType
 from datetime import datetime
@@ -10,20 +14,24 @@ from pyspark.mllib.evaluation import RegressionMetrics, RankingMetrics
 from pyspark.ml.stat import Correlation
 from pyspark.ml.feature import VectorAssembler
 
-def print_linear_regression_summary(lr_model):
-    print("Coefficients: " + str(lr_model.coefficients))
-    print("Intercept: " + str(lr_model.intercept))
-    return str(lr_model.coefficients),str(lr_model.intercept)
+def print_linear_regression_summary(linear_model):
+    coefficients = str(linear_model.coefficients)
+    intercept = str(linear_model.intercept)
+    print("** LIST OF REGRESSION COEFFICIENTS **")
+    print("- Coefficients: " + str(coefficients))
+    print("- Intercept: " + str(intercept))
 
-def print_training_summary(trainingSummary):
-    print("*** LINEAR MODEL ***")
-    print("RMSE (train): ", trainingSummary.rootMeanSquaredError)
-    print("R2 (train): ", trainingSummary.r2)
-    print("Adjusted R2 (train): ", trainingSummary.r2adj)
-    return trainingSummary.rootMeanSquaredError,trainingSummary.r2,trainingSummary.r2adj
+    return coefficients, intercept
 
-def print_test_summary(test_result):
-    print("RMSE (test): ", test_result.rootMeanSquaredError)
-    print("R2 (test): ", test_result.r2)
-    print("Adjusted R2 (test): ", test_result.r2adj)
-    return test_result.rootMeanSquaredError,test_result.r2,test_result.r2adj
+def print_summary(df_results):    
+    evaluator = RegressionEvaluator().setLabelCol("ArrDelay").setPredictionCol("prediction")
+    rmse = evaluator.evaluate(df_results, {evaluator.metricName: "rmse"})
+    r2 = evaluator.evaluate(df_results, {evaluator.metricName: "r2"})
+    # NOT SURE IF R2ADJ IS AVAILABLE IN THIS EVALUATION (IF NOT, REMOVE IT)
+    r2adj = evaluator.evaluate(df_results, {evaluator.metricName: "r2adj"})
+
+    print("- RMSE: ", rmse)
+    print("- R2: ", r2)
+    print("- Adjusted R2 (train): ", r2adj)
+    
+    return rmse, r2, r2adj
